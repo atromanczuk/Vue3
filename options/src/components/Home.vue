1e8cd2c1040a0f13b7b04cf0af66fb02a69fccbd
Tamara Romanczuk <tromanczuk@intecsoft.com.ar>
2022-03-26 16:29:48 -0300

    inicio: watch

--- options/src/components/Home.vue
+++ options/src/components/Home.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>{{ text }} {{ contador }} {{ obj }}</div>
+  <div>{{ obj.counter }}</div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { reactive, watch } from "vue";
 
 export default {
   setup() {
-    const text = ref("Holis");
-    const contador = ref(0);
+    // const text = ref("Holis");
+    // const contador = ref(0);
     const obj = reactive({ counter: 0 });
 
     setInterval(() => obj.counter++, 500);
 
+    watch(
+      () => obj.counter, //de esta manera conoce el valor más reciente
+      (valor, anterior) => {
+        console.log(valor, anterior);
+      }
+    );
+
     return {
-      text,
-      contador,
+      //   text,
+      //   contador,
       obj,
     };
   },
